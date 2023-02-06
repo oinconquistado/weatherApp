8afe35b253d82e4388581f857c53c4a7e61908dc
Neto <ssneto88@gmail.com>
2023-02-06 18:41:05 -0300

    feat: PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import * as path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
