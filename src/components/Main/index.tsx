a562e8fc71f4fa3ad0a38043a93f1dc45dfec825
Neto <ssneto88@gmail.com>
2023-02-07 10:48:04 -0300

    feat: relatives values for Main Component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,7 @@
 const Main = () => {
   return (
-    <div className='grid place-items-center h-[430px] mt-[60px] w-[430px]'>
-      <div className='grid place-items-center w-[380px] h-[287px]'>
+    <div className='grid place-items-center h-[46.1vh] mt-[6.4vh] w-screen'>
+      <div className='grid place-items-center w-[88.4vw] h-[30.8vh]'>
         <img src='./graph/sun.svg' alt='sun' />
         <div>
           <h1 className='font-bold text-mineshaft-900 dark:text-mineshaft-100 text-[40px] font-bold'>
