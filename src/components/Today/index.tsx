aebbf4185417f57a1b0f9398427067a75f73b254
Neto <ssneto88@gmail.com>
2023-02-07 10:57:34 -0300

    feat: relatives values for Today Component

--- src/components/Today/index.tsx
+++ src/components/Today/index.tsx
@@ -2,15 +2,15 @@ import IconItem from "../IconItem";
 
 const Today = () => {
   return (
-    <div className='mt-[100px]'>
-      <div className='grid place-items-center bg-mineshaft-100 text-mineshaft-800 dark:text-mineshaft-200 dark:bg-mineshaft-600 rounded-[20px] h-[85px] w-[390px]'>
-        <div className='grid grid-cols-2 grid-rows-2 h-[50px] w-[350px]'>
+    <div className='mt-[10.7vh]'>
+      <div className='grid place-items-center bg-mineshaft-100 text-mineshaft-800 dark:text-mineshaft-200 dark:bg-mineshaft-600 rounded-[1.25rem] h-[9.1vh] w-[90.7vw]'>
+        <div className='grid grid-cols-2 grid-rows-2 h-[5.4vh] w-[81.4vw]'>
           <div>
             <p className='font-bold'>Today</p>
           </div>
           <div className='flex items-center justify-end'>
             <div>
-              <p className='text-[15px] text-right'>29º max</p>
+              <p className='text-[.9375rem] text-right'>29º max</p>
             </div>
             <div>
               <div className='mx-2'>
@@ -18,12 +18,12 @@ const Today = () => {
               </div>
             </div>
             <div>
-              <p className='text-[15px] text-right'>26º min</p>
+              <p className='text-[.9375rem] text-right'>26º min</p>
             </div>
           </div>
           <div></div>
           <div>
-            <p className='text-[14px] text-mineshaft-400 dark:text-mineshaft-300 text-right'>
+            <p className='text-[.875rem] text-mineshaft-400 dark:text-mineshaft-300 text-right'>
               Feels like 32º
             </p>
           </div>
