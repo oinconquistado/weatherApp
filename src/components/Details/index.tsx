7d0616e463dc03f0a24e7234e2271cae37631610
Neto <ssneto88@gmail.com>
2023-02-07 11:01:41 -0300

    feat: relatives values for Details Component

--- src/components/Details/index.tsx
+++ src/components/Details/index.tsx
@@ -3,9 +3,9 @@ import { iconsPath } from "@/data/iconsPath";
 
 const Details = () => {
   return (
-    <div className='mt-[20px]'>
-      <div className='grid place-items-center bg-mineshaft-100 text-mineshaft-900 dark:bg-mineshaft-600 dark:text-mineshaft-200 rounded-[20px] h-[110px] w-[390px]'>
-        <div className='grid gap-y-[12px] gap-x-[40px] grid-rows-2 grid-cols-2 w-[340px] h-[72px]'>
+    <div className='mt-[2.1vh]'>
+      <div className='grid place-items-center bg-mineshaft-100 text-mineshaft-900 dark:bg-mineshaft-600 dark:text-mineshaft-200 rounded-[1.25rem] h-[11.8vh] w-[90.7vw]'>
+        <div className='grid gap-y-[.75rem] gap-x-[2.5rem] grid-rows-2 grid-cols-2 w-[79.1vw] h-[7.7vh]'>
           {iconsPath.map(({ path, name }, index) => (
             <ClimateItem key={index} path={path} name={name} />
           ))}
